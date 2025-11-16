432805ac58b8ad5ad42abd61dbebaec6f565adf3
Кирилл <kirillvolk@MacBook-Air-Kirill.local>
2025-11-16 16:49:43 +0300

    2 step

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,15 @@
 plugins {
     id("java")
-    id("org.springframework.boot") version "3.2.0"
-    id("io.spring.dependency-management") version "1.1.4"
+    id("application")
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
+
+application {
+    mainClass.set("hexlet.code.App")
+}
 
 java {
     sourceCompatibility = JavaVersion.VERSION_21
@@ -16,12 +20,8 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-    implementation("org.jsoup:jsoup:1.17.1")
-    runtimeOnly("com.h2database:h2")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("io.javalin:javalin:6.1.6")
+    implementation("org.slf4j:slf4j-simple:2.0.13")
 }
 
 tasks.test {
